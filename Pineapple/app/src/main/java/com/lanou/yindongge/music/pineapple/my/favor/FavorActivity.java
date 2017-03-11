555b92eb162f4ee0ac3b8fcd919c10716941b4c5
dllo <dllo@xiaoA.local>
2017-03-11 14:43:37 +0800

    收藏   适配

--- Pineapple/app/src/main/java/com/lanou/yindongge/music/pineapple/my/favor/FavorActivity.java
+++ Pineapple/app/src/main/java/com/lanou/yindongge/music/pineapple/my/favor/FavorActivity.java
@@ -1,29 +1,47 @@
 package com.lanou.yindongge.music.pineapple.my.favor;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.content.Intent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.lanou.yindongge.music.pineapple.R;
+import com.lanou.yindongge.music.pineapple.base.BaseActivity;
 import com.lanou.yindongge.music.pineapple.bean.FavorEntity;
+import com.lanou.yindongge.music.pineapple.detail.PlayActivity;
 import com.litesuits.orm.LiteOrm;
 
 import java.util.List;
 
-public class FavorActivity extends AppCompatActivity {
+public class FavorActivity extends BaseActivity implements AdapterView.OnItemClickListener{
+
+    private ListView lv;
+    private List<FavorEntity> favorEntities;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_favor);
+    public int getLayoutId() {
+        return R.layout.activity_favor;
+    }
 
-        RecyclerView favorRv = (RecyclerView)findViewById(R.id.favor_rv);
+    @Override
+    public void initData() {
         LiteOrm liteOrm = LiteOrm.newSingleInstance(this, "video.db");
         FavorAdapter favorAdapter = new FavorAdapter(this);
-        List<FavorEntity> dataFavor = liteOrm.query(FavorEntity.class);
-        favorAdapter.setDataFavor(dataFavor);
-        favorRv.setAdapter(favorAdapter);
-        favorRv.setLayoutManager(new LinearLayoutManager(this));
+        lv = (ListView) findViewById(R.id.listView);
+        favorEntities = liteOrm.query(FavorEntity.class);
+        favorAdapter.setList(favorEntities);
+        lv.setAdapter(favorAdapter);
+        lv.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        String url = favorEntities.get(i).getUrl();
+        Intent intent = new Intent(this, PlayActivity.class);
+        intent.putExtra("url", url);
+        startActivity(intent);
+        Toast.makeText(this, "点点点", Toast.LENGTH_SHORT).show();
     }
 }
+
--- Pineapple/app/src/main/java/com/lanou/yindongge/music/pineapple/my/favor/FavorAdapter.java
+++ Pineapple/app/src/main/java/com/lanou/yindongge/music/pineapple/my/favor/FavorAdapter.java
@@ -1,16 +1,18 @@
 package com.lanou.yindongge.music.pineapple.my.favor;
 
 import android.content.Context;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.lanou.yindongge.music.pineapple.R;
 import com.lanou.yindongge.music.pineapple.bean.FavorEntity;
-import com.lanou.yindongge.music.pineapple.net.ImageManagerFactory;
+import com.lanou.yindongge.music.pineapple.net.GlideManager;
+import com.lanou.yindongge.music.pineapple.util.ScreenSizeUtils;
+import com.lanou.yindongge.music.pineapple.util.ScreenState;
 
 import java.util.List;
 
@@ -18,49 +20,66 @@ import java.util.List;
  * Created by dllo on 17/3/10.
  */
 
-public class FavorAdapter extends RecyclerView.Adapter<FavorAdapter.FavorViewHolder> {
-
+public class FavorAdapter extends BaseAdapter {
     private Context context;
-    List<FavorEntity> dataFavor;
+    private List<FavorEntity> list;
 
     public FavorAdapter(Context context) {
         this.context = context;
     }
 
-    public void setDataFavor(List<FavorEntity> dataFavor) {
-        this.dataFavor = dataFavor;
+    public void setList(List<FavorEntity> list) {
+        this.list = list;
         notifyDataSetChanged();
     }
 
     @Override
-    public FavorViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.item_favor, null);
-        FavorViewHolder holder = new FavorViewHolder(view);
-        return holder;
+    public int getCount() {
+        return list != null ? list.size() : 0;
     }
 
     @Override
-    public void onBindViewHolder(FavorViewHolder holder, int position) {
-        holder.favorTitleTv.setText(dataFavor.get(position).getTitle());
-        holder.favorAuthorTv.setText(dataFavor.get(position).getAuthor());
-        ImageManagerFactory.getImageManager(ImageManagerFactory.GLIDE).loadImageView(context,
-                dataFavor.get(position).getImgStr(), holder.favorIv);
+    public Object getItem(int i) {
+        return list != null ? list.get(i) : null;
     }
 
     @Override
-    public int getItemCount() {
-        return dataFavor != null ? dataFavor.size() : 0;
+    public long getItemId(int i) {
+        return i;
     }
 
-    class FavorViewHolder extends RecyclerView.ViewHolder{
-        ImageView favorIv;
-        TextView favorTitleTv;
-        TextView favorAuthorTv;
-        public FavorViewHolder(View itemView) {
-            super(itemView);
-            favorIv = (ImageView)itemView.findViewById(R.id.favor_iv);
-            favorTitleTv = (TextView)itemView.findViewById(R.id.favor_title_tv);
-            favorAuthorTv = (TextView)itemView.findViewById(R.id.favor_author_tv);
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        FavorHolder holder = null;
+        if (view == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.item_favor, viewGroup, false);
+            holder = new FavorHolder(view);
+            view.setTag(holder);
+        } else {
+            holder = (FavorHolder) view.getTag();
+        }
+
+        ViewGroup.LayoutParams lp = holder.imageView.getLayoutParams();
+        lp.width = ScreenSizeUtils.getSreen(context, ScreenState.WIDTH) / 5 * 2;
+        lp.height = ScreenSizeUtils.getSreen(context, ScreenState.HEIGHT) / 6;
+        holder.imageView.setLayoutParams(lp);
+
+
+        GlideManager.getGlideManager().loadImageView(context, list.get(i).getImgStr(), holder.imageView);
+        holder.titleTv.setText(list.get(i).getTitle().toString());
+        holder.channalTv.setText(list.get(i).getAuthor());
+        return view;
+    }
+
+    class FavorHolder {
+        ImageView imageView;
+        TextView titleTv, channalTv;
+
+        public FavorHolder(View view) {
+            imageView = (ImageView) view.findViewById(R.id.favor_iv);
+            titleTv = (TextView) view.findViewById(R.id.favor_tv);
+            channalTv = (TextView) view.findViewById(R.id.channal_tv);
         }
     }
 }
+
